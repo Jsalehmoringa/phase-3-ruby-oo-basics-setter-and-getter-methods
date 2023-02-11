57865c3bbdb6e1e07e3c5d3ea428163fae336991
jamil <jamil.saleh@student.moringaschool.com>
2023-02-11 07:14:00 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(result)
+        @name = result
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(data)
+        @breed = data
+    end
+
+    def breed 
+        @breed
+    end
 
 end
